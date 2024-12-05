c325562bbc4cf8ac7283994d02c1e4383b8ec778
JosineyJr <josineyjunior14@gmail.com>
2024-12-05 11:30:28 -0300

    feature: hambuger menu for mobile devices

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { AccuracyService } from '../../../core/accuracy/accuracy.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  expanded = false;
+
   constructor(public readonly accuracyService: AccuracyService) {}
+
+  expand() {
+    this.expanded = !this.expanded;
+    console.log(this.expanded);
+  }
 }
